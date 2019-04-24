4925e938d675c4f016743569b9b3294c718024e8
zdkpm4444 <1042910420@qq.com>
2019-04-24 13:46:44 +0800

    test

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * @author micheal
+ * @version V1.0
+ * @Package PACKAGE_NAME
+ * @date 2019/4/24 13:42
+ * @Copyright 公司
+ */
+public class HelloWorld {
+    public static void main(String args[]){
+        System.out.println("HiGit");
+        System.out.println("First");
+    }
+}
